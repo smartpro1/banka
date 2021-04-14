afcbc051e2bbe1aa00a8923c8666ab14464bc77a
smartpro1 <akeni.promise@yahoo.com>
2021-04-14 14:57:14 +0100

    add react build files for deployment

---
+++ src/main/resources/static/precache-manifest.5a460702ba0b0d639c39e44c0a60404d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4ffb7b905e8fe22e72a56e9951e807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "773461ab13a37f42fe10",
+    "url": "/static/css/main.45b982a8.chunk.css"
+  },
+  {
+    "revision": "da06bfafa21fdf3209fc",
+    "url": "/static/js/2.1c6d46c7.chunk.js"
+  },
+  {
+    "revision": "38d43c6ba36865fb0cc07e2539b0465a",
+    "url": "/static/js/2.1c6d46c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773461ab13a37f42fe10",
+    "url": "/static/js/main.8f0ee478.chunk.js"
+  },
+  {
+    "revision": "1c77aa808e6221cec94e",
+    "url": "/static/js/runtime-main.063be002.js"
+  },
+  {
+    "revision": "a8f0959c32719f15298e2dc61cb42028",
+    "url": "/static/media/error_404.a8f0959c.jpg"
+  },
+  {
+    "revision": "d20dee073224f4a79d8b00143c8bf9f9",
+    "url": "/static/media/img1.d20dee07.PNG"
+  },
+  {
+    "revision": "77510da4b79fe22fe6ea79d470844ca1",
+    "url": "/static/media/img2.77510da4.PNG"
+  }
+]);
